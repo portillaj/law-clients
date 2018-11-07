ec27e011d152d01538cd8f882008a87170f9964a
jasonportilla <jasonportilla@portillaj.local>
2018-11-06 20:37:11 -0500

    added authentication

---
+++ auth.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+exports.authenticate = (email, password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findOne({ email });
+
+      // Match Password
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if(err) throw err;
+        if(isMatch) {
+          resolve(user);
+        } else {
+          // password did not match
+          reject('Authentication Failed');
+        }
+      });
+    } catch(err) {
+      // Email not found
+      reject('Authentication Failed');
+    }
+  });
+};
